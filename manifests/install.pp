105f2994369291f87e3c6c3c1726c52df82fa240
root <root@sankar.puppet-master.com>
2017-10-11 15:29:11 +0530

    PARAM MASTER

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,9 @@
-class ssh::install {
-  package { 'apache2':
+class ssh::install (
+  String $package_name = $::ssh::package_name,
+){
+  package { 'package_name':
     ensure => present,
+    name   => $package_name,
   }
 }
 
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,10 @@
-class ssh::service {
-  service { 'apache2':
+class ssh::service (
+  String $service_name = $::ssh::service_name,
+) {
+  service { 'ssh-service':
     ensure     => running,
+    name       => $service_name,
     enable     => true,
     hasstatus  => true,
     hasrestart =>true,
   }
-}
